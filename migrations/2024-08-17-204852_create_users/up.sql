8ae2dbbc6f0c10eeaa3a4815de363fc33dfaacdd
blastrider <guimaxali@gmail.com>
2024-08-17 22:49:55 +0200

    rempli les script sql up et down

--- migrations/2024-08-17-204852_create_users/up.sql
+++ migrations/2024-08-17-204852_create_users/up.sql
@@ -1,1 +1,11 @@
 -- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    password_hash VARCHAR NOT NULL,
+    address VARCHAR,
+    phone_number VARCHAR,
+    ssn VARCHAR
+);
+
