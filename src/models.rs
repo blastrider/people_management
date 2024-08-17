08395e16ec0969ee6515568bcccb881b61175968
blastrider <guimaxali@gmail.com>
2024-08-17 23:27:07 +0200

    ajoute un structure pour modifier un user

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,7 @@
 use crate::schema::users;
 use serde::{Deserialize, Serialize};
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
 
 #[derive(Queryable, Serialize)]
 pub struct User {
@@ -13,7 +15,7 @@ pub struct User {
 }
 
 #[derive(Insertable, Deserialize)]
-#[table_name = "users"]
+#[diesel(table_name = users)]
 pub struct NewUser {
     pub first_name: String,
     pub last_name: String,
@@ -23,3 +25,13 @@ pub struct NewUser {
     pub ssn: Option<String>,
 }
 
+#[derive(AsChangeset, Deserialize)]
+#[diesel(table_name = users)]
+pub struct UpdateUser {
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub password_hash: Option<String>,
+    pub address: Option<String>,
+    pub phone_number: Option<String>,
+    pub ssn: Option<String>,
+}
