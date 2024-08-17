d98d86695a7ace485d22713b450bb955c07254d0
blastrider <guimaxali@gmail.com>
2024-08-17 22:56:00 +0200

    ajoute dep diesel_migrations

--- src/db.rs
+++ src/db.rs
@@ -1,17 +1,19 @@
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use rocket_sync_db_pools::{database, diesel};
+use diesel_migrations::{MigrationHarness, EmbeddedMigrations};
 
 #[database("user_db")]
 pub struct DbConn(diesel::PgConnection);
 
-embed_migrations!();
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 pub async fn run_migrations(rocket: rocket::Rocket<rocket::Build>) -> rocket::Rocket<rocket::Build> {
     let conn = DbConn::get_one(&rocket).await
         .expect("database connection");
-    conn.run(|c| embedded_migrations::run(c)).await
-        .expect("can run migrations");
+    conn.run(|c| {
+        c.run_pending_migrations(MIGRATIONS).expect("migrations to run");
+    }).await;
     rocket
 }
 
