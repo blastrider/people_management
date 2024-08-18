06a35502291c572607cbc22dd280a58a844f75c8
blastrider <guimaxali@gmail.com>
2024-08-19 00:54:32 +0200

    ajoute le schema pour les comptes partagés

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,15 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    shared_accounts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        account_id -> Int4,
+        role -> Varchar,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         first_name -> Varchar,
@@ -11,3 +20,10 @@ diesel::table! {
         ssn -> Nullable<Varchar>,
     }
 }
+
+diesel::joinable!(shared_accounts -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    shared_accounts,
+    users,
+);
