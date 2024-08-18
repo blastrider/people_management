b58e43aab4a3ab6d7dd6d4a065e3528c97a58bb0
blastrider <guimaxali@gmail.com>
2024-08-19 00:51:13 +0200

    ajoute vérification si utilisateur existe déjà avec meme ssn

--- src/routes/users/create.rs
+++ src/routes/users/create.rs
@@ -1,20 +1,40 @@
-use rocket::serde::json::Json;
-use rocket::response::status::BadRequest;
 use crate::db::DbConn;
 use crate::models::{NewUser, User};
 use diesel::prelude::*;
+use diesel::result::Error;
+use rocket::response::status::BadRequest;
+use rocket::serde::json::Json;
 
 #[post("/users", data = "<new_user>")]
-pub async fn create_user(conn: DbConn, new_user: Json<NewUser>) -> Result<Json<User>, BadRequest<String>> {
+pub async fn create_user(
+    conn: DbConn,
+    new_user: Json<NewUser>,
+) -> Result<Json<User>, BadRequest<String>> {
     use crate::schema::users::dsl::*;
 
-    let new_user = new_user.into_inner();
-    conn.run(move |c| {
-        diesel::insert_into(users)
-            .values(&new_user)
-            .get_result(c)
-            .map(Json)
-            .map_err(|e| BadRequest(Some(format!("Error: {}", e)).unwrap_or_default()))
-    }).await
-}
+    let mut new_user = new_user.into_inner();
+    let ssn_clone = new_user.ssn.clone(); // Clonez la valeur de ssn
 
+    // Vérifier si un utilisateur avec le même SSN existe déjà
+    let existing_user = conn
+        .run(move |c| users.filter(ssn.eq(ssn_clone)).first::<User>(c))
+        .await;
+
+    match existing_user {
+        Ok(_) => Err(BadRequest(
+            "User with the same SSN already exists".to_string(),
+        )),
+        Err(Error::NotFound) => {
+            // Créer l'utilisateur car il n'existe pas déjà
+            let created_user = conn
+                .run(move |c| diesel::insert_into(users).values(&new_user).get_result(c))
+                .await;
+
+            match created_user {
+                Ok(user) => Ok(Json(user)),
+                Err(e) => Err(BadRequest(format!("Error creating user: {}", e))),
+            }
+        }
+        Err(e) => Err(BadRequest(format!("Database error: {}", e))),
+    }
+}
