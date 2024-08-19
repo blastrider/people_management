2595f6d6133b4a99450d8f8968708fc6347f2c3e
blastrider <guimaxali@gmail.com>
2024-08-19 22:10:43 +0200

    ajoute le champ active Option bool

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,8 @@ use crate::schema::{shared_accounts, users};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Serialize)]
+#[derive(Queryable, Selectable, Serialize)]
+#[diesel(table_name = users)]
 pub struct User {
     pub id: i32,
     pub first_name: String,
@@ -11,9 +12,10 @@ pub struct User {
     pub address: Option<String>,
     pub phone_number: Option<String>,
     pub ssn: Option<String>,
+    pub active: Option<bool>,
 }
 
-#[derive(Insertable, Deserialize)]
+#[derive(Insertable, Deserialize, Selectable)]
 #[diesel(table_name = users)]
 pub struct NewUser {
     pub first_name: String,
@@ -22,9 +24,10 @@ pub struct NewUser {
     pub address: Option<String>,
     pub phone_number: Option<String>,
     pub ssn: Option<String>,
+    pub active: bool,
 }
 
-#[derive(AsChangeset, Deserialize)]
+#[derive(AsChangeset, Deserialize, Selectable)]
 #[diesel(table_name = users)]
 pub struct UpdateUser {
     pub first_name: Option<String>,
@@ -33,9 +36,10 @@ pub struct UpdateUser {
     pub address: Option<String>,
     pub phone_number: Option<String>,
     pub ssn: Option<String>,
+    pub active: bool,
 }
 
-#[derive(Insertable, Deserialize)]
+#[derive(Insertable, Deserialize, Selectable)]
 #[diesel(table_name = shared_accounts)]
 pub struct NewSharedAccount {
     pub user_id: i32,
@@ -43,7 +47,7 @@ pub struct NewSharedAccount {
     pub role: String,
 }
 
-#[derive(Queryable, Serialize)]
+#[derive(Queryable, Serialize, Selectable)]
 pub struct SharedAccount {
     pub id: i32,
     pub user_id: i32,
